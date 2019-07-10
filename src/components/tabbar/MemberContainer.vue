6fef669db568a4c58c962099af5a6441b394dfe1
carrot417727510 <carrotjavascript@163.com>
2019-07-10 18:24:40 +0800

    first write

---
+++ src/components/tabbar/MemberContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>MemberContainer</h3>
+  </div>
+</template>
+
+<script>
+import { Toast } from "mint-ui";
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    Toast({
+      message: "呀呀呀",
+      duration: "2000"
+    });
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
